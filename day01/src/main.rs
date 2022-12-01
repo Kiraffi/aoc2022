dcf711702426b7572c49e08a7c8655820fb97e61
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2022-12-01 22:31:31 +0200

    simplify reading lines by embedding the data file

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,47 +1,52 @@
 use std::mem::swap;
 
-const FILE_PATH:&str = "../data/day01.txt";
-
 fn main()
 {
+    const RUN_AMOUNT:u32 = 1;
+    let data = include_str!("../../data/day01.txt");
     let now = std::time::Instant::now();
-    day01();
-    println!("Day1 duration: {}us", now.elapsed().as_micros());
+    for _ in 0..RUN_AMOUNT
+    {
+        day01(false, &data);
+    }
+    println!("Day1 duration: {}us", now.elapsed().as_micros() as f32 / RUN_AMOUNT as f32);
 }
 
-fn day01()
+fn day01(print_outcome: bool, content: &str)
 {
     const NUMBER_AMOUNT:usize = 3;
     let mut max_amounts: [i32; NUMBER_AMOUNT] = [0; NUMBER_AMOUNT];
     let mut current_amount = 0;
 
-    common_lib::read_line_by_line(FILE_PATH,
-        |line: &str|
+    for line in content.lines()
+    {
+        if line.len() != 0
         {
-            if line.len() != 0
-            {
-                current_amount += line.parse::<i32>().unwrap();
-            }
-            else
-            {
-                for n in 0..NUMBER_AMOUNT
-                {
-                    if max_amounts[n] < current_amount
-                    {
-                        swap(&mut max_amounts[n], &mut current_amount);
-                    }
-                }
-                current_amount = 0;
-            }
-            return true;
+            current_amount += line.parse::<i32>().unwrap();
         }
-    );
+        else
+        {
+            for n in 0..NUMBER_AMOUNT
+            {
+                if max_amounts[n] < current_amount
+                {
+                    swap(&mut max_amounts[n], &mut current_amount);
+                }
+            }
+            current_amount = 0;
+        }
+
+    }
 
     let mut sum_of_maxes = 0;
     for value in max_amounts
     {
         sum_of_maxes += value;
     }
-    println!("Day 1-1: Highest: {}", max_amounts[0]);
-    println!("Day 1-2: Sum of highest {} values: {}", NUMBER_AMOUNT, sum_of_maxes);
+
+    if print_outcome
+    {
+        println!("Day 1-1: Highest: {}", max_amounts[0]);
+        println!("Day 1-2: Sum of highest {} values: {}", NUMBER_AMOUNT, sum_of_maxes);
+    }
 }

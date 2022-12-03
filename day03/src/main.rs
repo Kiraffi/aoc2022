da63d4f10a4763a0c505561ad2469dc63c99ea19
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2022-12-03 14:45:47 +0200

    reworked day03 for more fun version

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,4 @@
+
 const RUN_AMOUNT:u32 = 1;
 
 fn main()
@@ -6,40 +7,36 @@ fn main()
     let _data = include_str!("../../data/day03.txt");
 
     let now = std::time::Instant::now();
-    for _ in 0..RUN_AMOUNT
+    for _ in 0..RUN_AMOUNT - 1
     {
-        day03_1(true, &_data);
-        day03_2(true, &_data);
+        day03_1(false, &_data);
+        day03_2(false, &_data);
     }
+    day03_1(true, &_data);
+    day03_2(true, &_data);
     println!("Day03 duration: {}us", now.elapsed().as_micros() as f32 / RUN_AMOUNT as f32);
 }
 
-fn get_priority(line: &str) -> u8
+fn calculate_str_value(line: &[u8]) -> u64
 {
-    let half = line.len() / 2;
-    let line_bytes = line.as_bytes();
-    let left_chars = &line_bytes[0..half];
-    let right_chars = &line_bytes[half..];
-    for left in left_chars
+    let mut value: u64 = 0;
+    for n in 0..line.len()
     {
-        if right_chars.contains(left)
-        {
-            return *left;
-        }
+        // bitwise or for the priority score
+        value |= 1u64 << get_priority_score(line[n]);
     }
-    return 0;
+    return value;
 }
 
-fn get_priority_score(priority: u8) -> u32
+fn get_priority_score(priority: u8) -> u8
 {
-    if priority >= 'a' as u8 && priority <= 'z' as u8
-    {
-        return (priority - 'a' as u8) as u32 + 1;
-    }
-    else
-    {
-        return (priority - 'A' as u8) as u32 + 27;
-    }
+    let value = (priority - 'A' as u8) as u8 + 1u8;
+    let value_bit = value & 32u8;          // Get the bit from lower case letters ('a' = 33, 'A' = 1).
+    let value_bit = value_bit >> 5u8;      // Bit shift it to smallest value.
+    let value_bit = value_bit ^ 1u8;       // Swap the bit.
+    let value = value & 31u8;              // Get the lower 5 bits ('A' = 1, 'a' = 1, was 33).
+    let value = value + value_bit * 26u8;  // Multiply the bit by 26 and add it to value
+    return value;
 }
 
 fn day03_1(print_outcome: bool, content: &str)
@@ -47,8 +44,13 @@ fn day03_1(print_outcome: bool, content: &str)
     let mut total_score: u32 = 0;
     for line in content.lines()
     {
-        let priority = get_priority(line);
-        total_score += get_priority_score(priority);
+        let half = line.len() / 2;
+        let line_bytes = line.as_bytes();
+        let left_chars = &line_bytes[0..half];
+        let right_chars = &line_bytes[half..];
+
+        let value: u64 = calculate_str_value(left_chars) & calculate_str_value(right_chars);
+        total_score += value.trailing_zeros();
     }
     if print_outcome
     {
@@ -72,21 +74,16 @@ fn day03_2(print_outcome: bool, content: &str)
     // while lines.peek() != None
     while let Some(l0) = lines.next()
     {
+        let value: u64 = calculate_str_value(l0.as_bytes());
+
         let l1 = get_str(lines.next());
+        let value = value & calculate_str_value(l1.as_bytes());
+
         let l2 = get_str(lines.next());
+        let value = value & calculate_str_value(l2.as_bytes());
 
-        let line0 = l0.as_bytes();
-        let line1 = l1.as_bytes();
-        let line2 = l2.as_bytes();
-
-        for priority0 in line0
-        {
-            if line1.contains(priority0) && line2.contains(priority0)
-            {
-                total_score += get_priority_score(*priority0);
-                break;
-            }
-        }
+//        total_score = total_score.wrapping_add(value as u32);
+        total_score += value.trailing_zeros();
     }
     if print_outcome
     {

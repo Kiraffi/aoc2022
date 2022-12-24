6f9f4cd06f74d85b8ba0679d3069fdaae4e819de
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2022-12-24 12:52:40 +0200

    day 24

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{VecDeque, HashSet};
+
 
 const RUN_AMOUNT:u32 = 1;
 const DAY_STR: &'static str = "24";
@@ -14,52 +16,172 @@ fn main()
         _ = part_b(&_data);
     }
     */
-    println!("Day {}-1: CHANGE THIS: {}", DAY_STR, part_a(&DATA));
-    println!("Day {}-2: CHANGE THIS: {}", DAY_STR, part_b(&DATA));
+    println!("Day {}-1: Through the blizzard time: {}", DAY_STR, part_a(&DATA));
+    println!("Day {}-2: Through the blizzard time and back and through again: {}", DAY_STR, part_b(&DATA));
     println!("Day {} duration: {}us", DAY_STR, now.elapsed().as_micros() as f32 / RUN_AMOUNT as f32);
 }
 
-#[derive(Clone)]
-struct Point
+struct Blizzard
 {
-    x: i64,
-    y: i64
+    x: u8,
+    y: u8,
+    dir: u8,
+    _padding: u8
 }
 
-impl Point
+fn _print_map(map: &Vec<Vec<char>>)
 {
-    fn min(a: &Point, b: &Point) -> Point
+    for l in map
     {
-        Point{x: std::cmp::min(a.x, b.x), y: std::cmp::min(a.y, b.y)}
+        for c in l
+        {
+            print!("{}", c);
+        }
+        println!("");
     }
-    fn max(a: &Point, b: &Point) -> Point
+    println!("");
+}
+
+fn simulate(start: (u8, u8), start_time: u32, blizzards: &Vec<Blizzard>, end: (u8, u8), map: &Vec<Vec<char>>) -> u32
+{
+    let mut posses: VecDeque<(u8, u8, u32)> = VecDeque::new();
+    let mut seen: HashSet<(u8, u8, u32)> = HashSet::new();
+    let map_size = (map[0].len() as u8, map.len() as u8);
+    posses.push_back((start.0, start.1, start_time));
+    'outer: while posses.len() > 0
     {
-        Point{x: std::cmp::max(a.x, b.x), y: std::cmp::max(a.y, b.y)}
+        let pos = posses.pop_front().unwrap();
+        if pos.0 >= map_size.0 || pos.1 >= map_size.1
+        {
+            continue;
+        }
+        if map[pos.1 as usize][pos.0 as usize] == '#'
+        {
+            continue;
+        }
+        if seen.contains(&pos)
+        {
+            continue;
+        }
+        seen.insert(pos);
+        let time = pos.2;
+
+        if pos.0 == end.0 && pos.1 == end.1
+        {
+            return time;
+        }
+        //let mut new_map = map.clone();
+        for b in blizzards
+        {
+            let mut x = b.x as i64;
+            let mut y = b.y as i64;
+            let _c = match b.dir
+            {
+                0 => { x += time as i64; '>'},
+                1 => { y += time as i64; 'v'},
+                2 => { x -= time as i64; '<'},
+                3 => { y -= time as i64; '^'},
+                _ => {'.'}
+            };
+            let x = (x.rem_euclid(map_size.0 as i64 - 2) + 1) as u8;
+            let y = (y.rem_euclid(map_size.1 as i64 - 2) + 1) as u8;
+            assert!(x > 0 && y > 0 && x < map_size.0 - 1 && y < map_size.1 - 1);
+            if x == pos.0 && y == pos.1
+            {
+                continue 'outer;
+            }
+            //new_map[y as usize][x as usize] = _c;
+        }
+        //new_map[pos.1 as usize][pos.0 as usize] = '@';
+        //println!("Time: {}", time);
+        //_print_map(&new_map);
+        if pos.0 > 0
+        {
+            posses.push_back((pos.0 - 1, pos.1, time + 1));
+        }
+        if pos.1 > 0
+        {
+            posses.push_back((pos.0, pos.1 - 1, time + 1));
+        }
+        if pos.0 < map_size.0
+        {
+            posses.push_back((pos.0 + 1, pos.1, time + 1));
+        }
+        if pos.1 < map_size.1
+        {
+            posses.push_back((pos.0, pos.1 + 1, time + 1));
+        }
+        posses.push_back((pos.0, pos.1, time + 1));
     }
+    return !0;
+}
+
+fn parse(content: &'static str) -> (Vec<Blizzard>, (u8, u8), (u8, u8), Vec<Vec<char>>)
+{
+    let mut blizzards: Vec<Blizzard> = Vec::new();
+    let lines = content.lines().collect::<Vec<&str>>();
+    let start = lines[0].find('.').unwrap();
+    let end = lines[lines.len() - 1].find('.').unwrap();
+    let mut map: Vec<Vec<char>> = Vec::new();
+
+    for row in 0..lines.len() as u8
+    {
+        let mut line: Vec<char> = Vec::new();
+        for (col, &c) in lines[row as usize].as_bytes().iter().enumerate()
+        {
+            line.push('.');
+            match c as char
+            {
+                '>' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 0, _padding: 0} )},
+                'v' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 1, _padding: 0} )},
+                '<' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 2, _padding: 0} )},
+                '^' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 3, _padding: 0} )},
+                '#' => { line[col] = '#' },
+                _ => {}
+            }
+        }
+        map.push(line);
+    }
+    let start = (start as u8, 0u8);
+    let end = (end as u8, lines.len() as u8 - 1);
+    return (blizzards, start, end, map);
 }
 
 #[test]
 fn part_a_test()
 {
     let value = part_a(&_TEST_DATA);
-    assert_eq!(value, 0);
+    assert_eq!(value, 18);
 }
 
 fn part_a(content: &'static str) -> i64
 {
-    return 0;
+    let (blizzards, start, end, map) = parse(content);
+
+    let fastest = simulate(
+        start,
+        0,
+        &blizzards,
+        end,
+        &map);
+    return fastest as i64;
 }
 
 #[test]
 fn part_b_test()
 {
     let value = part_b(&_TEST_DATA);
-    assert_eq!(value, 0);
+    assert_eq!(value, 54);
 }
 
 fn part_b(content: &'static str) -> i64
 {
-    return 0;
+    let (blizzards, start, end, map) = parse(content);
+
+    let fastest = simulate(start,0, &blizzards, end, &map);
+    let fastest = simulate(end, fastest as u32, &blizzards, start, &map);
+    let fastest = simulate(start, fastest as u32, &blizzards, end, &map);
+    return fastest as i64;
 }
 
 

7522dccd9a5cee4e1cabfc80889f1d0b02410fe2
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2022-12-24 19:53:01 +0200

    make day24 run faster

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,5 +1,4 @@
-use std::collections::{VecDeque, HashSet};
-
+use std::collections::HashSet;
 
 const RUN_AMOUNT:u32 = 1;
 const DAY_STR: &'static str = "24";
@@ -23,8 +22,8 @@ fn main()
 
 struct Blizzard
 {
-    x: u8,
-    y: u8,
+    x: i8,
+    y: i8,
     dir: u8,
     _padding: u8
 }
@@ -45,85 +44,92 @@ fn _print_map(map: &Vec<Vec<char>>)
 #[derive(Copy, Clone, Eq, Hash, PartialEq)]
 struct Stamp
 {
-    time: u16,
-    x: u8,
-    y: u8
+    x: i8,
+    y: i8
 }
 
-fn simulate(start: (u8, u8), start_time: u16, blizzards: &Vec<Blizzard>, end: (u8, u8), map: &Vec<Vec<char>>) -> u16
+fn try_add(stamp: &Stamp, map: &Vec<Vec<char>>, seen: &mut HashSet<Stamp>, posses: &mut Vec<Stamp>)
 {
-    let mut posses: VecDeque<Stamp> = VecDeque::new();
-    let mut seen: HashSet<Stamp> = HashSet::new();
-    let map_size = (map[0].len() as u8, map.len() as u8);
-    posses.push_back(Stamp{time: start_time, x: start.0, y: start.1});
-    'outer: while posses.len() > 0
+    if stamp.x < 0
+        || stamp.y < 0
+        || stamp.x as usize >= map[0].len()
+        || stamp.y as usize >= map.len()
     {
-        let stamp = posses.pop_front().unwrap();
-        if stamp.x >= map_size.0 || stamp.y >= map_size.1
-        {
-            continue;
-        }
-        if map[stamp.y as usize][stamp.x as usize] == '#'
-        {
-            continue;
-        }
-        if seen.contains(&stamp)
-        {
-            continue;
-        }
-        seen.insert(stamp);
+        return;
+    }
+    if map[stamp.y as usize][stamp.x as usize] != '.'
+    {
+        return;
+    }
+    if seen.contains(stamp)
+    {
+        return;
+    }
+    seen.insert(*stamp);
+    posses.push(*stamp);
+}
 
-        if stamp.x == end.0 && stamp.y == end.1
-        {
-            return stamp.time;
-        }
-        //let mut new_map = map.clone();
+fn simulate(start: (i8, i8), start_time: u16, blizzards: &Vec<Blizzard>, end: (i8, i8), map: &Vec<Vec<char>>) -> u16
+{
+    let mut posses: Vec<Stamp> = Vec::new();
+    let map_size = (map[0].len() as i8, map.len() as i8);
+    posses.push(Stamp{x: start.0, y: start.1});
+    let mut time = start_time;
+    while posses.len() > 0
+    {
+        let mut new_posses: Vec<Stamp> = Vec::new();
+
+        let mut new_map = map.clone();
         for b in blizzards
         {
             let mut x = b.x as i64;
             let mut y = b.y as i64;
-            let _c = match b.dir
+            let c = match b.dir
             {
-                0 => { x += stamp.time as i64; '>'},
-                1 => { y += stamp.time as i64; 'v'},
-                2 => { x -= stamp.time as i64; '<'},
-                3 => { y -= stamp.time as i64; '^'},
-                _ => {'.'}
+                0 => { x += time as i64 + 1; '>'},
+                1 => { y += time as i64 + 1; 'v'},
+                2 => { x -= time as i64 + 1; '<'},
+                3 => { y -= time as i64 + 1; '^'},
+                _ => { assert!(false); '.'}
             };
-            let x = (x.rem_euclid(map_size.0 as i64 - 2) + 1) as u8;
-            let y = (y.rem_euclid(map_size.1 as i64 - 2) + 1) as u8;
+            let x = (x.rem_euclid(map_size.0 as i64 - 2) + 1) as usize;
+            let y = (y.rem_euclid(map_size.1 as i64 - 2) + 1) as usize;
             //assert!(x > 0 && y > 0 && x < map_size.0 - 1 && y < map_size.1 - 1);
-            if x == stamp.x && y == stamp.y
-            {
-                continue 'outer;
-            }
-            //new_map[y as usize][x as usize] = _c;
+            new_map[y][x] = c;
         }
+
+
         //new_map[pos.1 as usize][pos.0 as usize] = '@';
         //println!("Time: {}", time);
         //_print_map(&new_map);
-        if stamp.x > 0
+        let mut seen: HashSet<Stamp> = HashSet::new();
+        //let mut new_map_tmp = new_map.clone();
+
+        while posses.len() > 0
         {
-            posses.push_back(Stamp{time: stamp.time + 1, x: stamp.x - 1, y: stamp.y});
+            let stamp = posses.pop().unwrap();
+            //println!{"{}, {}", stamp.x, stamp.y};
+            //new_map_tmp[stamp.y as usize][stamp.x as usize] = '@';
+            if stamp.x == end.0 && stamp.y == end.1
+            {
+                return time;
+            }
+
+            try_add(&Stamp{x: stamp.x + 0, y: stamp.y + 0}, &new_map, &mut seen, &mut new_posses);
+            try_add(&Stamp{x: stamp.x + 1, y: stamp.y + 0}, &new_map, &mut seen, &mut new_posses);
+            try_add(&Stamp{x: stamp.x - 1, y: stamp.y + 0}, &new_map, &mut seen, &mut new_posses);
+            try_add(&Stamp{x: stamp.x + 0, y: stamp.y + 1}, &new_map, &mut seen, &mut new_posses);
+            try_add(&Stamp{x: stamp.x + 0, y: stamp.y - 1}, &new_map, &mut seen, &mut new_posses);
         }
-        if stamp.y > 0
-        {
-            posses.push_back(Stamp{time: stamp.time + 1, x: stamp.x, y: stamp.y - 1});
-        }
-        if stamp.x < map_size.0
-        {
-            posses.push_back(Stamp{time: stamp.time + 1, x: stamp.x + 1, y: stamp.y});
-        }
-        if stamp.y < map_size.1
-        {
-            posses.push_back(Stamp{time: stamp.time + 1, x: stamp.x, y: stamp.y + 1});
-        }
-        posses.push_back(Stamp{time: stamp.time + 1, x: stamp.x, y: stamp.y});
+        //_print_map(&new_map_tmp);
+
+        posses = new_posses;
+        time += 1;
     }
     return !0;
 }
 
-fn parse(content: &'static str) -> (Vec<Blizzard>, (u8, u8), (u8, u8), Vec<Vec<char>>)
+fn parse(content: &'static str) -> (Vec<Blizzard>, (i8, i8), (i8, i8), Vec<Vec<char>>)
 {
     let mut blizzards: Vec<Blizzard> = Vec::new();
     let lines = content.lines().collect::<Vec<&str>>();
@@ -131,7 +137,7 @@ fn parse(content: &'static str) -> (Vec<Blizzard>, (u8, u8), (u8, u8), Vec<Vec<c
     let end = lines[lines.len() - 1].find('.').unwrap();
     let mut map: Vec<Vec<char>> = Vec::new();
 
-    for row in 0..lines.len() as u8
+    for row in 0..lines.len() as i8
     {
         let mut line: Vec<char> = Vec::new();
         for (col, &c) in lines[row as usize].as_bytes().iter().enumerate()
@@ -139,18 +145,18 @@ fn parse(content: &'static str) -> (Vec<Blizzard>, (u8, u8), (u8, u8), Vec<Vec<c
             line.push('.');
             match c as char
             {
-                '>' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 0, _padding: 0} )},
-                'v' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 1, _padding: 0} )},
-                '<' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 2, _padding: 0} )},
-                '^' => { blizzards.push(Blizzard {x: col as u8 - 1, y: row - 1, dir: 3, _padding: 0} )},
+                '>' => { blizzards.push(Blizzard {x: col as i8 - 1, y: row - 1, dir: 0, _padding: 0} )},
+                'v' => { blizzards.push(Blizzard {x: col as i8 - 1, y: row - 1, dir: 1, _padding: 0} )},
+                '<' => { blizzards.push(Blizzard {x: col as i8 - 1, y: row - 1, dir: 2, _padding: 0} )},
+                '^' => { blizzards.push(Blizzard {x: col as i8 - 1, y: row - 1, dir: 3, _padding: 0} )},
                 '#' => { line[col] = '#' },
                 _ => {}
             }
         }
         map.push(line);
     }
-    let start = (start as u8, 0u8);
-    let end = (end as u8, lines.len() as u8 - 1);
+    let start = (start as i8, 0i8);
+    let end = (end as i8, lines.len() as i8 - 1);
     return (blizzards, start, end, map);
 }
 
